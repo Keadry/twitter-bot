ae0720a9685ce0a3de759556a518dad948f07344
Keadry <ahmetbolat6167@gmail.com>
2023-08-10 09:33:17 +0300

    Pytest Added

--- twitterTest.py
+++ twitterTest.py
@@ -1,32 +1,32 @@
-import unittest
+import pytest
 from selenium import webdriver
 from twitterLogin import TwitterLogIn  
 from twitterHome import TwitterHome
 
-class TestTwitter(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.driver = webdriver.Chrome()
-        cls.driver.implicitly_wait(10)
+@pytest.fixture(scope="class")
+def setup_teardown(request):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
+    request.cls.driver = driver
 
-    def testTwitterHome(self):
-        word = "your_words"
-        twitter_home = TwitterHome(self.driver)
-        tweet_text = twitter_home.search(word)
-        self.assertIsNotNone(tweet_text, "Arama sonuçlarından tweet metni alınamadı.")
+    yield
 
-    def testTwitterLogin(self):
+    driver.quit()
+
+@pytest.mark.usefixtures("setup_teardown")
+class TestTwitter:
+    def test_twitter_login(self):
         username = "your_username"
         password = "your_password"
         twitter_login = TwitterLogIn(username, password, self.driver)
         twitter_login.signIn()
         expected_url = f"https://twitter.com/home"
         actual_url = self.driver.current_url
-        self.assertEqual(expected_url, actual_url, "Oturum açma başarısız veya yönlendirilen sayfa yanlış.")
+        assert expected_url == actual_url, "Oturum açma başarısız veya yönlendirilen sayfa yanlış."
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_twitter_home(self):
+        word = "your_word"
+        twitter_home = TwitterHome(self.driver)
+        tweet_text = twitter_home.search(word)
+        assert tweet_text is not None, "Arama sonuçlarından tweet metni alınamadı."
